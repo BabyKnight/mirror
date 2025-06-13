c7a04ceac0c9fee95327c1615ed0013952e23281
Vincent Hu <vincent.guohua.hu@gmail.com>
2025-06-13 12:13:00 +0800

    add impliment on the view and temnplate for samples

--- mirror/core/views.py
+++ mirror/core/views.py
@@ -1,5 +1,8 @@
+from django.forms.models import model_to_dict
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import Sample, Image, Task
+
 
 def ping(request):
     x_forward_for = request.META.get('HTTP_X_FORWARD_FOR')
@@ -15,4 +18,40 @@ def dummy_view(request, *args, **kwargs):
 
 
 def dashboard(request):
-    return render(request, 'index.html', {'msg':'Welcome'})
+    context = {}
+    samples = {
+        "total": 60,
+        "changes": -0.12
+        }
+    platforms = {
+        "total": 30,
+        "changes": 2
+        }
+    failed_cases = {
+        "total": 20,
+        "changes": -0.18
+        }
+    tasks = {
+        "total": 25,
+        "changes": 0
+        }
+
+    # search for Task
+    # search for Samples
+    samples = Sample.objects.all()
+    sample_list = []
+
+    for i in samples:
+        sample_info = model_to_dict(i)
+        sample_list.append(sample_info)
+
+    # search for Images
+
+    context = {
+        "samples": samples,
+        "platforms": platforms,
+        "failed_cases": failed_cases,
+        "tasks": tasks,
+        "sample_list": sample_list
+        }
+    return render(request, 'index.html', context)
